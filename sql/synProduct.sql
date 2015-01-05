be0004acba7e5eb3bc4cf02d5bc54eccea91c321
qudong <qudong@d881c268-5c81-4d69-b6ac-ed4e9099e0f2>
2015-01-05 04:05:14 +0000

    git-svn-id: http://192.168.2.96/svn/avit/02_product/ADS/2_branch/ADS_V100R001C006_/02_Src/mysql@14518 d881c268-5c81-4d69-b6ac-ed4e9099e0f2

---
+++ sql/synProduct.sql
@@ -0,0 +1,101 @@
+--同步双向产品
+CREATE DEFINER = CURRENT_USER PROCEDURE `synProduct`()
+    NOT DETERMINISTIC
+    CONTAINS SQL
+    SQL SECURITY DEFINER
+    COMMENT ''
+BEGIN
+update `t_productinfo` m join t_productinfo_temp n on m.`PRODUCT_ID`=n.`PRODUCT_ID`
+ and m.`networkID`=n.`networkID` set m.`PRODUCT_NAME`=n.`PRODUCT_NAME`;
+commit;
+delete FROM t_productinfo  where (`PRODUCT_ID`,`networkID`) not in (
+select  PRODUCT_ID,networkID from  `t_productinfo_temp`
+);
+commit;
+insert into t_productinfo  select n.* from t_productinfo_temp n  where (n.`PRODUCT_ID`,n.`networkID`) not IN(select PRODUCT_ID,networkID from  `t_productinfo` );
+commit;
+   
+END;
+
+--同步双向频道信息
+CREATE DEFINER = CURRENT_USER PROCEDURE `synChannelNPVR`()
+    NOT DETERMINISTIC
+    CONTAINS SQL
+    SQL SECURITY DEFINER
+    COMMENT ''
+BEGIN
+
+update `t_channelinfo_npvr` m join t_channelinfo_npvr_temp n on m.`CHANNEL_ID`=n.`CHANNEL_ID`
+ and m.`networkID`=n.`networkID` set m.`CHANNEL_NAME`=n.`CHANNEL_NAME`;
+commit;
+delete FROM t_channelinfo_npvr  where (`CHANNEL_ID`,`networkID`) not in (
+select  CHANNEL_ID,networkID from  `t_channelinfo_npvr_temp`
+);
+commit;   
+insert into t_channelinfo_npvr  select n.* from t_channelinfo_npvr_temp n  where (n.`CHANNEL_ID`,n.`networkID`) not IN(select CHANNEL_ID,networkID from  `t_channelinfo_npvr` );
+commit;
+
+END;
+
+--同步用户级别信息
+CREATE DEFINER = CURRENT_USER PROCEDURE `synUserRank`()
+    NOT DETERMINISTIC
+    CONTAINS SQL
+    SQL SECURITY DEFINER
+    COMMENT ''
+BEGIN
+
+update `t_user_rank` m join t_user_rank_temp n on m.`USER_RANK_CODE`=n.`USER_RANK_CODE`
+ and m.`areacode`=n.`areacode` set m.`USER_RANK_NAME`=n.`USER_RANK_NAME`;
+commit;
+delete FROM t_user_rank  where (`USER_RANK_CODE`,`areacode`) not in (
+select  USER_RANK_CODE,areacode from  `t_user_rank_temp`
+);
+commit;
+insert into t_user_rank  select n.* from t_user_rank_temp n  where (n.`USER_RANK_CODE`,n.`areacode`) not IN(select USER_RANK_CODE,areacode from  `t_user_rank` );
+commit;   
+END;
+
+--同步行业类别信息
+CREATE DEFINER = CURRENT_USER PROCEDURE `synUserIndustryCategory`()
+    NOT DETERMINISTIC
+    CONTAINS SQL
+    SQL SECURITY DEFINER
+    COMMENT ''
+BEGIN
+
+update `t_user_industry_category` m join t_user_industry_category_temp n on m.`USER_INDUSTRY_CATEGORY_CODE`=n.`USER_INDUSTRY_CATEGORY_CODE`
+ and m.`areacode`=n.`areacode` set m.`USER_INDUSTRY_CATEGORY_VALUE`=n.`USER_INDUSTRY_CATEGORY_VALUE`;
+commit;
+delete FROM t_user_rank  where (`USER_INDUSTRY_CATEGORY_CODE`,`areacode`) not in (
+select  USER_INDUSTRY_CATEGORY_CODE,areacode from  `t_user_industry_category_temp`
+);
+commit;   
+insert into t_user_industry_category  select n.* from t_user_industry_category_temp n  where (n.`USER_INDUSTRY_CATEGORY_CODE`,n.`areacode`) not IN(select USER_INDUSTRY_CATEGORY_CODE,areacode from  `t_user_industry_category` );
+commit;
+END;
+
+--同步区域信息
+CREATE DEFINER = CURRENT_USER PROCEDURE `synLocationCode`()
+    NOT DETERMINISTIC
+    CONTAINS SQL
+    SQL SECURITY DEFINER
+    COMMENT ''
+BEGIN
+
+update `t_location_code` m join t_location_code_temp n on m.`LOCATIONCODE`=n.`LOCATIONCODE`
+ and m.`areacode`=n.`areacode` set m.`LOCATIONNAME`=n.`LOCATIONNAME` and m.PARENTLOCATION=n.PARENTLOCATION and m.LOCATIONTYPE=n.LOCATIONTYPE;
+commit;
+delete FROM t_location_code  where (`LOCATIONCODE`,`areacode`) not in (
+select  LOCATIONCODE,areacode from  `t_location_code_temp`
+);
+commit;   
+insert into t_location_code  select n.* from t_location_code_temp n  where (n.`LOCATIONCODE`,n.`areacode`) not IN(select LOCATIONCODE,areacode from  `t_location_code` );
+commit;
+END;
+
+
+
+
+
+
